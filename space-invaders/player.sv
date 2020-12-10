21e3201ffa68971de8d3707758900b407f678507
YeiKT140 <kant.tangchart@gmail.com>
2020-12-11 03:16:12 +0700

    Update player.sv

--- space-invaders/player.sv
+++ space-invaders/player.sv
@@ -104,44 +104,5 @@ module player(
             default:    state_next = IDLE;
         endcase
   end
-  
-    // always_ff @ (posedge Reset or posedge frame_clk )
-    // begin: Move_player
-    //     if (Reset)  // Asynchronous Reset
-    //     begin 
-    //       player_X_Motion <= 0; //player_X_Step;
-    //       player_X_Pos <= player_X_Center;
-    //     end           
-    //     else 
-    //     begin 					  
-    //         if ( (player_X_Pos) >= player_X_Max )
-    //                     begin
-    //                         player_X_Motion <= 0;
-    //                         need_move <= 0;
-    //                     end					  
-    //         else if ( (player_X_Pos) <= player_X_Min )
-    //                     begin
-    //                       player_X_Motion <= 0;
-    //                       need_move <= 1;
-    //                     end
-    //         else if (keycode == 8'h04 && need_move != 1) // A
-    //                     begin
-    //                       player_X_Motion <= (~(player_X_Step)+1'b1);
-    //                       need_move <= 2;
-    //                     end
-    //         else if( keycode == 8'h07 && need_move != 0) // D
-    //                 begin
-    //                   player_X_Motion <= player_X_Step;
-    //                   need_move <= 2;
-    //                 end
-    //         else
-    //             begin
-    //               player_X_Motion <= 0;
-    //               need_move <= 2;
-    //             end
-    //         player_X_Pos <= (player_X_Pos + player_X_Motion);
-    //     end
-    // end       
-    // assign Player_X = player_X_Pos;
 
 endmodule
