7a53aabc163ae071b7df3a83ccdf001913eb6d27
YeiKT140 <kant.tangchart@gmail.com>
2020-12-11 20:29:48 +0700

    Updated starting position

--- space-invaders/player.sv
+++ space-invaders/player.sv
@@ -1,26 +1,25 @@
 module player(
     input logic Reset, frame_clk, Clk, 
     input logic [7:0] keycode,
-    input logic start,
-    input logic [9:0] player_initial_x, player_initial_y,
+    // input logic [9:0] player_initial_x, player_initial_y,
     input logic [9:0] DrawX, DrawY,
     output logic player_on,
     output logic [23:0] player_color
 );
 
-    logic [9:0] player_X_Pos, player_Y_Pos, player_X_Motion, player_size;
-    logic need_move; //2 can go any direction, 1 for need to move right, 0 for need to move left.
     parameter [9:0] player_X_Center=320;  // Center position on the X axis 320
     parameter [9:0] player_X_Min=20;       // Leftmost point on the X axis Min = 0
     parameter [9:0] player_X_Max=600;     // Rightmost point on the X axis Max = 639
     parameter [9:0] player_X_Step= 1;      // Step size on the X axis
-    
-    logic [9:0] player_start_X, player_start_Y;
+    parameter [9:0] player_Y_Center = 420;
+
+
+    logic [9:0] player_start_X, player_start_Y; // position on screen
 
     int IMAGE_WIDTH = 10'd36; 
     int IMAGE_HEIGHT = 10'd40;
     
-    logic [18:0] pos; // position within memory array
+    logic [9:0] player_X_Pos, player_Y_Pos; // position on sprite
 
     enum {
         IDLE,       // awaiting start signal
@@ -28,46 +27,63 @@ module player(
         AWAIT_POS,  // await horizontal position
         DRAW,       // draw pixel
         NEXT_LINE   // prepare for next sprite line
-    }state, next_state;
+    } state, next_state;
    
-   always_ff @ (posedge frame_clk) begin
-        if(keycode == 8'h04 && player_start_X > 0)begin // A
-          player_start_X <= player_start_X - 1;
+   logic start_draw;
+
+   logic [18:0] read_address;
+
+  //  initial begin
+  //   player_start_X = player_X_Center;
+  //   player_start_Y = player_Y_Center;
+  //  end
+
+   always_comb begin // Check when to draw
+     if(DrawX == player_start_X + player_X_Pos && DrawY == player_start_Y + player_Y_Pos)
+        start_draw = 1'b1;
+      else
+        start_draw = 1'b0;
+   end
+
+   always_ff @ (posedge frame_clk) begin // MOTION
+        if(keycode == 8'h04 && player_start_X > 0) // A
+          player_start_X <= player_start_X - 5;
+        else if (keycode == 8'h07 && player_start_X < 563) // D
+          player_start_X <= player_start_X + 5;
+        else if (Reset) begin
+          player_start_X = player_X_Center;
+          player_start_Y = player_Y_Center;
         end
-        else if (keycode == 8'h07 && player_start_X < 563)begin // D
-          player_start_X <= player_start_X + 1;
-        end else
+        else
           player_start_X <= player_start_X;
    end
 
    player_spriteRAM player_inst(
         .data_In(5'b0),
         .write_address(19'b0),
-        .read_address(pos),
+        .read_address(read_address),
         .we(1'b0),
-        .Clk(Clk)
+        .Clk(frame_clk),
         .data_Out(player_color)
    );
-  logic start_moving;
-  assign start_moving = start;
-  always_ff @ (posedge Clk) begin
-    state <= state_next;
+  
+  always_ff @ (posedge frame_clk) begin
+    state <= next_state;
 
     if(state == START) begin
         player_Y_Pos <= 0;
-        player_X_Motion <= 0;
-        pos <= 0;
+        read_address <= 0;
         player_on <= 1'b0;
     end
 
     if(state == AWAIT_POS) begin
         player_X_Pos <= 0;
-        enemy_on <= 1'b1;
+        player_on <= 1'b0;
     end
 
     if(state == DRAW) begin
         player_X_Pos <= player_X_Pos + 1;
-        pos <= pos + 1;
+        read_address <= read_address + 1;
         player_on <= 1'b1;
     end
 
@@ -78,14 +94,11 @@ module player(
 
     if(Reset) begin
       state <= IDLE;
-      player_start_X = player_initial_x;
-      player_start_Y = player_initial_y;
       player_X_Pos <= 0;
       player_Y_Pos <= 0;
-      pos <= 0;
+      read_address <= 0;
       player_on <= 1'b0;            
     end
-
   end
 
   logic last_pixel, last_line;
@@ -97,12 +110,12 @@ module player(
   // determine next state
   always_comb begin
         case(state)
-            IDLE:       state_next = start ? START : IDLE;
-            START:      state_next = AWAIT_POS;
-            AWAIT_POS:  state_next = (player_initial_x == DrawX) ? DRAW : AWAIT_POS;
-            DRAW:       state_next = !last_pixel ? DRAW : (!last_line ? NEXT_LINE : IDLE);
-            NEXT_LINE:  state_next = AWAIT_POS;
-            default:    state_next = IDLE;
+            IDLE:       next_state = start_draw ? START : IDLE;
+            START:      next_state = AWAIT_POS;
+            AWAIT_POS:  next_state = (DrawX == player_X_Pos) ? DRAW : AWAIT_POS;
+            DRAW:       next_state = !last_pixel ? DRAW : (!last_line ? NEXT_LINE : IDLE);
+            NEXT_LINE:  next_state = AWAIT_POS;
+            default:    next_state = IDLE;
         endcase
   end
 
